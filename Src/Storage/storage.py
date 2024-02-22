f6ce69c7be9c817e757bf317097f26927193ddbe
Svetlana Rudova <rudovasvetlana570@gmail.com>
2024-02-22 16:24:24 +0800

    Create storage.py

---
+++ Src/Storage/storage.py
@@ -0,0 +1,61 @@
+#
+# Класс хранилище данных
+#
+class storage:
+    __data = {}
+    
+    
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(storage, cls).__new__(cls)
+        return cls.instance  
+    
+    @property
+    def data(self) -> dict:
+        """
+         Данные по моделям
+
+        Returns:
+            _type_: _description_
+        """
+        return self.__data
+
+ 
+    @staticmethod
+    def nomenclature_key():
+        """
+            Ключ для хранения номенклатуры
+        Returns:
+            _type_: _description_
+        """
+        return "nomenclature"
+
+  
+    @staticmethod
+    def group_key():
+        """
+            Списк номенклатурных групп
+        Returns:
+            _type_: _description_
+        """
+        return "group"
+      
+      
+
+    @staticmethod  
+    def unit_key():
+        """
+              Список единиц измерения
+        Returns:
+            _type_: _description_
+        """
+        return "unit"
+    
+    @staticmethod
+    def receipt_key():
+        """
+            Ключ для хранения рецептов
+        Returns:
+            _type_: _description_
+        """
+        return "receipt"
