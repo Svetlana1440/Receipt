eaf720c14d0f157e3091fb8f03d1a91b81d8ad1c
Svetlana Rudova <rudovasvetlana570@gmail.com>
2024-02-22 16:23:34 +0800

    Create receipe_row_model.py

---
+++ Src/Models/receipe_row_model.py
@@ -0,0 +1,60 @@
+from Src.reference import reference
+from Src.Models.nomenclature_model import nomenclature_model
+from Src.Models.unit_model import unit_model
+from Src.exceptions import exception_proxy
+
+#
+# Класс описание одной строки рецепта
+#
+class receipe_model(reference):
+    __nomenclature: nomenclature_model = None
+    __size: int = 0
+    __unit: unit_model = None
+    
+    def __init__(self, _nomenclature: nomenclature_model, _size: int, _unit: unit_model):
+        exception_proxy.validate(_nomenclature, reference)
+        exception_proxy.validate(_unit, reference)
+         
+        self.__nomenclature = _nomenclature
+        self.__size = _size
+        self.__unit = _unit
+        
+        super().__init__( f"{_nomenclature.name} , {_unit.name} ")
+    
+    @property
+    def nomenclature(self):
+        """
+            Номенклатура
+        Returns:
+            _type_: _description_
+        """
+        return self.__nomenclature
+    
+    
+    @property
+    def size(self):
+        """
+            Размер
+
+        Returns:
+            _type_: _description_
+        """
+        return self.__size
+    
+    
+    @size.setter
+    def size(self, value: int):
+        self.__size = value
+    
+    
+    @property    
+    def unit(self):
+        """
+           Единица измерения
+
+        Returns:
+            _type_: _description_
+        """
+        return self.__unit    
+        
+    
