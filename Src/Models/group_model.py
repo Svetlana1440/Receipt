1bb198627ff49cfaaed458503aaf31800785d9cf
Svetlana Rudova <rudovasvetlana570@gmail.com>
2024-02-22 16:23:21 +0800

    Create group_model.py

---
+++ Src/Models/group_model.py
@@ -0,0 +1,18 @@
+from Src.reference import reference
+
+#
+# Модель группу номенклатуры
+# 
+class group_model(reference):
+    
+    
+    def create_group():
+        """
+        Фабричный метод. Создать группу по умолчанию
+
+        Returns:
+            _type_: _description_
+        """
+        item = group_model("Ингредиенты")
+        return item
+    
