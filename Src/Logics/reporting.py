cc4b459b6e0ced73a232cfd6211fcbc01dc15349
Svetlana Rudova <rudovasvetlana570@gmail.com>
2024-03-03 18:58:22 +0800

    Create reporting.py

---
+++ Src/Logics/reporting.py
@@ -0,0 +1,96 @@
+from abc import ABC
+from Src.settings import settings
+from Src.exceptions import exception_proxy, operation_exception
+
+
+#
+# Абстрактный класс для реализации отчетности
+#
+class reporting(ABC):
+    # Настройки
+    __settings: settings = None
+    # Набор данных
+    __data = {}
+    # Список полей
+    __fields = []    
+
+    
+    def __init__(self, _setting: settings, _data):
+        """
+
+        Args:
+            _setting (settings): Настройки
+            _data (_type_): Словарь с данными
+        """
+        
+        exception_proxy.validate(_setting, settings)
+        exception_proxy.validate(_data, dict)
+        
+        self.__data = _data
+        self.__settings = _setting
+        
+
+ 
+    def create(self, typeKey: str):
+        """
+            Сформировать отчет
+        Args:
+            typeKey (str): Ключ тип данных
+        """
+        exception_proxy.validate(typeKey, str)
+        self.__fields = self.build(typeKey, self.__data)
+        
+        return ""
+    
+        
+    
+    @staticmethod
+    def build( typeKey: str, data: dict) -> list:
+        """
+            Предобработка. Получить набор полей
+        Args:
+            typeKey (str): ключ в словаре_
+            data (dict): Данные - словарь
+
+        Returns:
+            list: список
+        """
+        
+        exception_proxy.validate(typeKey, str)
+        if data is None:
+            raise operation_exception("Набор данных не определен!")
+        
+        if len(data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        item = data[typeKey][0]
+        result = list(filter(lambda x: not x.startswith("_") and not x.startswith("create_")  , dir(item)))
+       
+        return result    
+    
+    def _build(self, typeKey: str) -> list:
+        """
+           Предобработка данных. Возвращает набор полей класса typeKey
+        Args:
+            typeKey (str): ключ для выборки данных
+        Returns:
+            list: список
+        """
+        return reporting.build(typeKey, self.__data)
+        
+        
+    @property    
+    def fields(self) -> list:
+        """
+        Набор полей от исходного объекта на основании которого формируем отчет
+        """    
+        return self.__fields    
+            
+    @property         
+    def data(self) -> dict:
+        """
+
+        Returns:
+            dict: словарь с данными
+        """
+        return self.__data    
